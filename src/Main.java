cd32c3336bbc1591fc6c45964f0557dec304ff7c
unknown <BT_3W2_03@BKTECH.LOCAL>
2022-11-10 12:27:56 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,115 @@
+public class Main {
+    public static void main(String[] args) {
+        /*for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int i2;
+        for (i2=1; i2<11; i2++) {
+            System.out.print(i2 + " ");
+        }
+        System.out.println();
+
+        for (int cd = 15; cd>=0; cd--) {
+            System.out.print(cd + " ");
+        }
+        System.out.println();
+
+        int cd2;
+        for (cd2 = 15; cd2>=0; cd2--) {
+            System.out.print(cd2 + " ");
+        }
+        System.out.println();
+
+        for (int odd = 1; odd <= 15; odd++) {
+            if (odd%2==1) {
+                System.out.print(odd + " ");
+            }
+        }
+        System.out.println();
+
+        for (int odd2 = 1; odd2 <= 15; odd2+=2) {
+            System.out.print(odd2 + " ");
+        }
+        System.out.println("\n--------------------------------");
+
+        for (int count = 5; count < 10; count++) {
+            System.out.print(count + " ");
+        }
+        System.out.println();
+
+        for (int i = 20; i >= 0; i = i - 3) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int sum = 0;
+        int x = 5;
+        for (int i = 0; i < 10 + x; i += 2) {
+            sum += i;
+        }
+        System.out.println(sum);
+        System.out.println();
+
+        int i=5;
+        while (i<10) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        int i = 20;
+        while (i>=0) {
+            System.out.print(i + " ");
+            i-=3;
+        }
+
+        for (int i=0; i<=100; i+=5) {
+            System.out.print(i + " ");
+        }
+
+        for (int i=10, j=40; i<=j+10; i+=5) {
+            System.out.print(i + " ");
+        }*/
+
+        /*for (int i = 4; i<=11; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n---------------------");
+
+        for (int i2 = 1; i2<=11-3; i2++) {
+            System.out.print(i2 + 3 + " ");
+        }*/
+
+        /*for (int i = 4; i <= 14; i+=2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n---------------------");
+
+        for (int i2 = 4; i2 <= 14; i2++) {
+            if (i2%2==0) {
+                System.out.print(i2 + " ");
+            }
+        }*/
+
+        /*for (int i = 0; i <= 100; i++) {
+            if (i%6==0 && i!=0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println("\n---------------------");
+
+        for (int i2 = 6; i2 <= 100; i2+=6) {
+            System.out.print(i2 + " ");*/
+
+        int num = 100;
+        for (int count = 1; count <= num; count++)
+        {
+            System.out.print(num + " ");
+            num = num / 2;
+        }
+
+    }
+}
